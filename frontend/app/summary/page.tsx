e8124726a6e11ab9b2eabb640f85fa835dc65436
yuandadhama <yuandadhamap@gmail.com>
2025-12-07 13:54:28 +0700

    update summary page to fix error deployment

--- frontend/app/summary/page.tsx
+++ frontend/app/summary/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams, useRouter } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
 import Button from "@/components/Button";
