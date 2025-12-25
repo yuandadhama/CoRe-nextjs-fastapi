1651955fc7fe6fa9f01631231dc347fafeb51b6a
yuandadhama <yuandadhamap@gmail.com>
2025-12-25 15:22:44 +0700

    allow all origins for CORS to test easily

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ origins = [frontend_url]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
